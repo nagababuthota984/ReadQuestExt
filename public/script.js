010344e41329e9602b2e771aa71e9735eb3433a7
Nagababu Thota <nagababu.t@technovert.com>
2024-08-06 15:28:18 +0530

    minor fixes

--- public/script.js
+++ public/script.js
@@ -28,7 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
   startButton.addEventListener("click", () => {
     document.getElementById('loader').style.display = 'block';
     buildFetchRequest(heading);
-    startButton.style.display = "none";
+    startButton.innerText = "generating...";
+    startButton.disabled = true;
     document.getElementById('loader').style.display = 'none';
   });
 
@@ -36,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
   async function buildFetchRequest(heading) {
     let encodedQuery = encodeURIComponent(heading);
     url = `${baseURL}?query=${encodedQuery}`;
-  
+
     try {
       const response = await fetch(url);
       let data = await response.text();
@@ -46,58 +47,76 @@ document.addEventListener("DOMContentLoaded", () => {
         .trim();
       quizData = JSON.parse(data);
       renderQuiz(quizData);
+      startButton.style.display = "none";
       submitBtn.style.display = "block";
     } catch (error) {
-      console.error("Error fetching quiz data:", error);
+      console.log("Error fetching quiz data:", error);
+      startButton.innerText = "Try again";
+      startButton.disabled = false;
     }
   }
-  
+
   //Rendering the quiz data
   function renderQuiz(quizData) {
     quizContainer.innerHTML = "";
     quizData.questions.forEach((question, index) => {
       const questionDiv = document.createElement("div");
       questionDiv.classList.add("question");
-  
+
       const questionTitle = document.createElement("h2");
       questionTitle.textContent = question.question;
       questionDiv.appendChild(questionTitle);
-  
+
       for (let i = 1; i <= 4; i++) {
         const optionDiv = document.createElement("div");
         optionDiv.id = `question[${index}]-[option${i}]`;
         optionDiv.classList.add("option");
-  
+
         const optionInput = document.createElement("input");
         optionInput.type = "radio";
         optionInput.name = `question${index}`;
         optionInput.value = question[`option${i}`];
         optionInput.id = `question${index}-option${i}`;
-  
+        optionInput.classList.add("radio-btn");
+
         const optionLabel = document.createElement("label");
         optionLabel.htmlFor = `question${index}-option${i}`;
         optionLabel.textContent = question[`option${i}`];
-  
+
         optionDiv.appendChild(optionInput);
         optionDiv.appendChild(optionLabel);
         questionDiv.appendChild(optionDiv);
         submitBtn.innerHTML = "Submit";
       }
-  
+
       quizContainer.appendChild(questionDiv);
     });
   }
-  
+
   submitBtn.addEventListener("click", () => {
     let score = 0;
     quizData.questions.forEach((question, index) => {
-      for(let i = 1; i <= 4; i++){
+      let optionValue = '';
+      let isIntegerCheck = false;
+      let correctOptionNumber = 0;
+      if (question.correctAnswer.startsWith('option')) {
+        optionValue = question.correctAnswer.slice(-1);
+        isIntegerCheck = true;
+        if (!isNaN(optionValue)) {
+          correctOptionNumber = parseInt(optionValue, 10);
+        }
+      }
+      else
+        optionValue = question.correctAnswer;
+      for (let i = 1; i <= 4; i++) {
         const optionDiv = document.getElementById(`question[${index}]-[option${i}]`);
         const option = document.getElementById(`question${index}-option${i}`);
-        if(question.correctAnswer == option.value)
+        console.log(option.value);
+        console.log(question.correctAnswer);
+        if ((!isIntegerCheck && question.correctAnswer == option.value) || (isIntegerCheck && correctOptionNumber == i))
           optionDiv.classList.add("correct-option");
-        if(option.checked){
-          if(question.correctAnswer != option.value)
+        if (option.checked) {
+          if ((!isIntegerCheck && question.correctAnswer != option.value) || (isIntegerCheck && correctOptionNumber !== i))
             optionDiv.classList.add("wrong-option");
           else
             score++;
@@ -105,8 +124,9 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
     resultContainer.textContent = `Your score: ${score}/${quizData.questions.length}`;
+    submitBtn.style.display = "none";
   });
-  
+
 });
 
 function getH1Element() {
